6b1205f456dbc6606f51174d689efe3402d20c31
karlaQuintanaR <s22120215@alumnos.itsur.edu.mx>
2025-09-19 22:50:53 -0600

    Se creo una viewModel, se corrigio para que se marcara la casilla de realizado, finalmente se concluyo la app.

--- app/src/main/java/com/example/basicstatecodelab/WellnessTasksList.kt
+++ app/src/main/java/com/example/basicstatecodelab/WellnessTasksList.kt
@@ -4,19 +4,26 @@ import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.runtime.remember
 
-
 @Composable
 fun WellnessTasksList(
     list: List<WellnessTask>,
+    onCheckedTask: (WellnessTask, Boolean) -> Unit,
     onCloseTask: (WellnessTask) -> Unit,
     modifier: Modifier = Modifier
 ) {
-    LazyColumn(modifier = modifier) {
+    LazyColumn(
+        modifier = modifier
+    ) {
         items(
             items = list,
             key = { task -> task.id }
         ) { task ->
-            WellnessTaskItem(taskName = task.label, onClose = { onCloseTask(task) })
+            WellnessTaskItem(
+                taskName = task.label,
+                checked = task.checked,
+                onCheckedChange = { checked -> onCheckedTask(task, checked) },
+                onClose = { onCloseTask(task) }
+            )
         }
     }
 }
